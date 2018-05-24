f29aae73372f79994e16ac9af23235936d913045
anandtiwarics <anandtiwarics@gmail.com>
2018-05-24 23:35:27 +0530

    Revert "RUN Update"
    This reverts commit 38c16ec1302048ba8e6e39ad2dcd72f2e47cad69.

--- run.sh
+++ run.sh
@@ -8,4 +8,5 @@ python manage.py makemigrations osintscan
 python manage.py makemigrations jiraticketing
 python manage.py makemigrations tools
 python manage.py migrate
+python manage.py process_tasks &
 python manage.py runserver 0.0.0.0:8000
