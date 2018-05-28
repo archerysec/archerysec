16d8d595175115ad1f7a4d27803d18fa13c83ea1
anand <anandtiwarics@gmail.com>
2018-05-28 15:55:36 +0530

    Update models.py

--- tools/models.py
+++ tools/models.py
@@ -4,14 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-# Create your models here.
+# SSLScan Model.
 class sslscan_result_db(models.Model):
     scan_id = models.TextField(blank=True, null=True)
     project_id = models.TextField(blank=True, null=True)
     scan_url = models.TextField(blank=True, null=True)
     sslscan_output = models.TextField(blank=True, null=True)
 
-
+# Nikto Model
 class nikto_result_db(models.Model):
     scan_id = models.TextField(blank=True, null=True)
     project_id = models.TextField(blank=True, null=True)
