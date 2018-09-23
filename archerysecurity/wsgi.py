f971200e5b1bf372f241d5c337f370cf3df58c66
anandtiwarics <anandtiwarics@gmail.com>
2018-09-23 22:29:54 +0530

    update

--- archerysecurity/wsgi.py
+++ archerysecurity/wsgi.py
@@ -10,10 +10,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "archerysecurity.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)
