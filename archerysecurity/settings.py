b7752027ebd61ade267d05648f912eae469cc325
anandtiwarics <anandtiwarics@gmail.com>
2018-09-23 23:43:03 +0530

    update

--- archerysecurity/settings.py
+++ archerysecurity/settings.py
@@ -138,7 +138,7 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-info',
@@ -183,5 +183,3 @@ REFRESH_TIMER = 5
 SETTINGS_EXPORT = [
     'REFRESH_TIMER',
 ]
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
