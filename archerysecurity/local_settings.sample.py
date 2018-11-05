24a7b442ae8d11af96a5b88ee1b70ebc88dc000d
Kenneth Belitzky <kenny@belitzky.com>
2018-11-05 10:26:18 -0300

    setup django debug toolbar

--- archerysecurity/local_settings.sample.py
+++ archerysecurity/local_settings.sample.py
@@ -14,6 +14,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEBUG = True
+
+INTERNAL_IPS = [
+    '172.26.0.1',
+    '127.0.0.1'
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
