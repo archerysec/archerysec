bc9df75e6e52ebb4bca4c4647f41bbc6539d4b82
Anand Tiwari <anandtiwarics@gmail.com>
2018-11-22 20:29:17 +0530

    Migrating data in Docker image

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,6 @@
 pip install -r requirements.txt
 yes yes | python manage.py collectstatic
+python manage.py makemigrations
 python manage.py migrate
 python manage.py initadmin
 python manage.py process_tasks &
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 pip install -r requirements.txt
 rm -rf /root/.cache
 yes yes | python manage.py collectstatic
+python manage.py makemigrations
 python manage.py migrate
 python manage.py initadmin
 python manage.py process_tasks &
