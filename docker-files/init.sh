cf472a1943aec78089bfe04a4395fc2331b103bc
Kenneth Belitzky <kenny@belitzky.com>
2018-11-25 13:42:32 -0300

    remove makemigrations from startup

--- docker-files/init.sh
+++ docker-files/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
-python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py initadmin
 
--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 pip install -r requirements.txt
 yes yes | python manage.py collectstatic
-python manage.py makemigrations
 python manage.py migrate
 python manage.py initadmin
