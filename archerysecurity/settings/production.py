0c8107f4f92d5deb31497a9bca82b63eeffaca83
Anand Tiwari <anandtiwarics@gmail.com>
2018-12-01 00:18:14 +0530

    update development env

--- archerysecurity/settings/production.py
+++ archerysecurity/settings/production.py
@@ -7,7 +7,7 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': 'db',
+        'HOST': os.getenv("DB_HOST"),
         'PORT': 5432,
     }
 }
