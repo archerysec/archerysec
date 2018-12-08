a8b81ca8b3e20befdc192feceb97658c161fe828
Kenneth Belitzky <kenny@belitzky.com>
2018-12-08 18:58:12 -0300

    Fix postgres availability

--- docker-files/init.sh
+++ docker-files/init.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
+
+# wait for Postgres to be available
+until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing migrations"
+exec $cmd
+
 python manage.py migrate --noinput
 python manage.py initadmin
 
