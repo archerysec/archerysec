7c801c92492ec25a2b3f0d1346efe770cd59190c
Kenneth Belitzky <kenny@belitzky.com>
2018-12-16 18:52:19 -0300

    control if usin sqlite or not

--- docker-files/init.sh
+++ docker-files/init.sh
@@ -3,13 +3,16 @@
 python manage.py collectstatic --noinput
 
 # wait for Postgres to be available
-until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+if [[ -z "${DB_HOST}" ]]; then
+  until PGPASSWORD=$DB_PASSWORD psql -h "$DB_HOST" -U "$DB_USER" -c '\q'; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 1
+  done
 
->&2 echo "Postgres is up - executing migrations"
-exec $cmd
+  >&2 echo "Postgres is up - executing migrations"
+  exec $cmd
+fi
+
 
 python manage.py migrate --noinput
 python manage.py initadmin
