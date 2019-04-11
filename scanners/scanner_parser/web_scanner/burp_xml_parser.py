b7910e4cd25bce1fe83c8fa1a34dd6f2be1b449e
Anand Tiwari <anandtiwarics@gmail.com>
2019-04-11 13:23:47 +0530

    fixed burp xml parser

--- scanners/scanner_parser/web_scanner/burp_xml_parser.py
+++ scanners/scanner_parser/web_scanner/burp_xml_parser.py
@@ -131,10 +131,36 @@ def burp_scan_data(root, project_id, scan_id):
                         res_dat = d.text
                         response_datas = base64.b64decode(res_dat)  # res_dat
 
-                    for key, items in met.iteritems():
+                    for key, items in met.items():
                         global methods
                         if key == "method":
                             methods = items
+
+            if data.tag == "issueBackground":
+                global issue_description
+                if data.text is None:
+                    issue_description = "NA"
+                else:
+                    issue_description = data.text
+            if data.tag == "remediationBackground":
+                global issue_remediation
+                if data.text is None:
+                    issue_remediation = "NA"
+                else:
+                    issue_remediation = data.text
+            if data.tag == "references":
+                global issue_reference
+                if data.text is None:
+                    issue_reference = "NA"
+                else:
+                    issue_reference = data.text
+            if data.tag == "vulnerabilityClassifications":
+                global issue_vulnerability_classifications
+                if data.text is None:
+                    issue_vulnerability_classifications = "NA"
+                else:
+                    issue_vulnerability_classifications = data.text
+
         global vul_col
         if severity == 'High':
             vul_col = "important"
@@ -148,7 +174,7 @@ def burp_scan_data(root, project_id, scan_id):
         vuln_id = uuid.uuid4()
 
         dup_data = name + path + severity
-        duplicate_hash = hashlib.sha256(dup_data).hexdigest()
+        duplicate_hash = hashlib.sha256(dup_data.encode('utf-8')).hexdigest()
 
         match_dup = burp_scan_result_db.objects.filter(
             dup_hash=duplicate_hash).values('dup_hash').distinct()
@@ -175,12 +201,6 @@ def burp_scan_data(root, project_id, scan_id):
 
         url = host + location
 
-        all_issue_definitions = burp_issue_definitions.objects.filter(issue_type_id=types)
-        for def_data in all_issue_definitions:
-            issue_description = def_data.description
-            issue_remediation = def_data.remediation
-            issue_vulnerability_classifications = def_data.vulnerability_classifications
-            issue_reference = def_data.reference
 
         try:
             data_dump = burp_scan_result_db(
@@ -209,7 +229,7 @@ def burp_scan_data(root, project_id, scan_id):
             )
             data_dump.save()
         except Exception as e:
-            print e
+            print(e)
     burp_all_vul = burp_scan_result_db.objects.filter(scan_id=scan_id).values('name', 'severity'
                                                                               ).distinct()
     total_vul = len(burp_all_vul)
@@ -230,5 +250,5 @@ def burp_scan_data(root, project_id, scan_id):
     try:
         email_notification.email_notify()
     except Exception as e:
-        print e
+        print(e)
     HttpResponse(status=201)
