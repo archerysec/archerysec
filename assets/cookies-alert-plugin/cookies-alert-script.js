837605c5957c9b88db90e51e9360d98d471cbb39
Anand Tiwari <anandtiwarics@gmail.com>
2019-05-11 14:13:45 +0530

    updated website

---
+++ assets/cookies-alert-plugin/cookies-alert-script.js
@@ -0,0 +1,2 @@
+
+jQuery(function(){var a=jQuery;a.cookiesDirective({explicitConsent:!1,position:"bottom",duration:9999,limit:0,message:a("input[name=cookieData]").attr("data-cookie-text"),fontFamily:"Arial",fontColor:"#424a4d",fontSize:"13px",backgroundColor:"#eaeff1",backgroundOpacity:"80"});a(".cookieText").find("a").css({color:"#424a4d","text-decoration":"underline"});a("input[name=cookieData]").remove()});
