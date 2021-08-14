aa337c3a43f73e377c30e12bb60665e9d1395b3c
Anand Tiwari <anandtiwarics@gmail.com>
2021-08-14 20:30:32 +0530

    settings test cases added

--- archerysettings/urls.py
+++ archerysettings/urls.py
@@ -21,7 +21,7 @@ from archerysettings import views
 app_name = "archerysettings"
 
 urlpatterns = [
-    path("settings/", views.setting, name="settings"),
-    path("del_setting/", views.del_setting, name="del_setting"),
-    path("email_setting/", views.email_setting, name="email_setting"),
+    path("settings/", views.Settings.as_view(), name="settings"),
+    path("del_setting/", views.DeleteSettings.as_view(), name="del_setting"),
+    path("email_setting/", views.EmailSetting.as_view(), name="email_setting"),
 ]
