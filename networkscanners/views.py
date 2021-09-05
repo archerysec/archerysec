e0e54638d74ff48494aba1eb9990e79595e9c880
Anand Tiwari <anandtiwarics@gmail.com>
2021-09-05 14:15:16 +0530

    django testing

--- networkscanners/views.py
+++ networkscanners/views.py
@@ -612,14 +612,14 @@ class NetworkScanDetails(APIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request):
+        jira_server = None
+        jira_username = None
+        jira_password = None
+        jira_projects = None
         vuln_id = request.GET["vuln_id"]
         scanner = request.GET["scanner"]
         jira_setting = jirasetting.objects.filter()
         user = request.user
-        jira_server = ""
-        jira_username = ""
-        jira_password = ""
-        jira_projects = ""
 
         for jira in jira_setting:
             jira_server = jira.jira_server
